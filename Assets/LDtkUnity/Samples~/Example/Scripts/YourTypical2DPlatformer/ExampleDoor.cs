080d3ec9234402cb322b88f7bf3eecec6dcd690c
Cameron <cameo221@gmail.com>
2020-11-14 15:23:21 -0800

    symbolic link expriment

---
+++ Assets/LDtkUnity/Samples~/Example/Scripts/YourTypical2DPlatformer/ExampleDoor.cs
@@ -0,0 +1,10 @@
+﻿using LDtkUnity.Runtime.FieldInjection;
+using UnityEngine;
+
+namespace LDtkUnity.Samples.Example.Scripts.YourTypical2DPlatformer
+{
+    public class ExampleDoor : MonoBehaviour
+    {
+        [LDtkField] public bool locked;
+    }
+}
