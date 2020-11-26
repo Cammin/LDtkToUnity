12a6ee81882708ac7f77b913ed689fbab3a592fd
Cameron <cameo221@gmail.com>
2020-11-26 00:13:10 -0800

    added a log check if an assigned tileset image was not set for read/write enabled

--- Assets/LDtkUnity/Runtime/UnityAssets/Tileset/LDtkTilesetAsset.cs
+++ Assets/LDtkUnity/Runtime/UnityAssets/Tileset/LDtkTilesetAsset.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 namespace LDtkUnity.Runtime.UnityAssets.Tileset
 {
     [HelpURL(LDtkHelpURL.TILESET_ASSET)]
-    [CreateAssetMenu(fileName = nameof(LDtkTilesetAsset), menuName = LDtkToolScriptableObj.SO_PATH + nameof(LDtkTilesetAsset), order = LDtkToolScriptableObj.SO_ORDER)]
-    public class LDtkTilesetAsset : LDtkAsset<Sprite> {}
+    [CreateAssetMenu(fileName = nameof(LDtkTilesetAsset),
+        menuName = LDtkToolScriptableObj.SO_PATH + nameof(LDtkTilesetAsset), order = LDtkToolScriptableObj.SO_ORDER)]
+    public class LDtkTilesetAsset : LDtkAsset<Sprite>
+    {
+        private void OnValidate()
+        {
+            if (!AssetExists) return;
+
+            if (!ReferencedAsset.texture.isReadable)
+            {
+                Debug.LogWarning($"Tileset \"{ReferencedAsset.texture.name}\" texture does not have Read/Write Enabled, is it enabled?", this);
+            }
+        }
+    }
 }
