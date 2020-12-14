a22ef999f00b2055470539decb76ef04bcd625eb
Cameron <cameo221@gmail.com>
2020-12-14 02:14:42 -0800

    made exclusive settable camera background component

---
+++ Assets/LDtkUnity/Runtime/BuildEvents/StaticEventRecievers/LDtkSettableCameraBackground.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LDtkUnity.Builders;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace LDtkUnity.BuildEvents
+{
+    //todo add helpurl
+    public class LDtkSettableCameraBackground : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        
+        private void OnEnable()
+        {
+            LDtkLevelBuilder.OnLevelBackgroundColorSet += UpdateBackgroundColor;
+        }
+
+        private void OnDisable()
+        {
+            LDtkLevelBuilder.OnLevelBackgroundColorSet -= UpdateBackgroundColor;
+        }
+
+        private void UpdateBackgroundColor(Color bgColor)
+        {
+            _camera.backgroundColor = bgColor;
+        }
+    }
+}
