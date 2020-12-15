7bde69c503937a1cbc70b58836c2d816959ee7d2
Cameron <cameo221@gmail.com>
2020-12-15 00:53:15 -0800

    made example for testing all possible fields

---
+++ Assets/LDtkUnity/Samples~/TestAllFields/Data/TestAllFields_Data/TestAllFields_Data_Enums.cs
@@ -0,0 +1,15 @@
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+
+namespace LDtkUnity.Enums
+{
+    /// <summary>
+    /// LDtk Enum for project TestAllFields_Data
+    /// </summary>
+    public enum SomeEnum
+    {
+        Alpha,
+        Beta,
+        Omega
+    }
+}
