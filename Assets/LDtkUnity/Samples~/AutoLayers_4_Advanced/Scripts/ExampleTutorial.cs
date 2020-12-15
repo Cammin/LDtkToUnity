e69cc397b3d0261bbcb3a8609c2028a3154808b6
Cameron <cameo221@gmail.com>
2020-12-15 00:52:36 -0800

    setup sample AutoLayers_4_Advanced

---
+++ Assets/LDtkUnity/Samples~/AutoLayers_4_Advanced/Scripts/ExampleTutorial.cs
@@ -0,0 +1,19 @@
+﻿using LDtkUnity.FieldInjection;
+using UnityEngine;
+
+namespace Samples.AutoLayers_4_Advanced
+{
+    public class ExampleTutorial : MonoBehaviour
+    {
+        [SerializeField] private TextMesh _textMesh = null;
+        
+        [LDtkField("text")] public string _label;
+        [LDtkField("color")] public Color _color;
+
+        public void UpdateValues()
+        {
+            _textMesh.text = _label;
+            _textMesh.color = _color;
+        }
+    }
+}
