a5d4b430e036afef83e3e5f2d06ca549a80c9baa
Cameron <cameo221@gmail.com>
2021-01-06 13:22:35 -0800

    further organization, set up one of the extension classes

--- Assets/LDtkUnity/Runtime/Data/Extensions/LDtkDataProjectExtensions.cs
+++ Assets/LDtkUnity/Runtime/Data/Extensions/LDtkDataProjectExtensions.cs
@@ -7,9 +7,9 @@ namespace LDtkUnity.Data
 {
     public static class LDtkDataProjectExtensions
     {
-        public static Color BgColor(this LDtkDataProject data) => data.bgColor.ToColor();
-        public static Color DefaultLevelBgColor(this LDtkDataProject data) => data.defaultLevelBgColor.ToColor();
-        public static Vector2 DefaultPivot(this LDtkDataProject data) => new Vector2(data.defaultPivotX, data.defaultPivotY);
-        public static Vector2Int WorldGridSize(this LDtkDataProject data) => new Vector2Int(data.worldGridWidth, data.worldGridHeight);
+        public static Color UnityBgColor(this LdtkJson data) => data.BgColor.ToColor();
+        public static Color UnityDefaultLevelBgColor(this LdtkJson data) => data.DefaultLevelBgColor.ToColor();
+        public static Vector2 UnityDefaultPivot(this LdtkJson data) => new Vector2((int)data.DefaultPivotX, (int)data.DefaultPivotY);
+        public static Vector2Int UnityWorldGridSize(this LdtkJson data) => new Vector2Int((int)data.WorldGridWidth, (int)data.WorldGridHeight);
     }
 }
