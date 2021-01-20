d79f7573d3181f8cd0a4ac04f3c8fc26e6ed3e1d
Cameron <cameo221@gmail.com>
2021-01-20 13:25:39 -0800

    new test code

--- Assets/Tests/Editor/FieldDeserializeTest.cs
+++ Assets/Tests/Editor/FieldDeserializeTest.cs
@@ -1,4 +1,5 @@
-﻿using LDtkUnity;
+﻿using System;
+using LDtkUnity;
 using LDtkUnity.Data;
 using Newtonsoft.Json;
 using NUnit.Framework;
@@ -8,22 +9,22 @@ namespace Tests.Editor
 {
     public class FieldDeserializeTest
     {
-        [Test] public void DeserializeFieldInt() => DeserializeField("Int");
-        [Test] public void DeserializeFieldFloat() => DeserializeField("Float");
-        [Test] public void DeserializeFieldBool() => DeserializeField("Bool");
-        [Test] public void DeserializeFieldString() => DeserializeField("String");
-        [Test] public void DeserializeFieldEnum() => DeserializeField("Enum");
-        [Test] public void DeserializeFieldColor() => DeserializeField("Color");
-        [Test] public void DeserializeFieldPoint() => DeserializeField("Point");
-        [Test] public void DeserializeFieldIntArray() => DeserializeField("IntArray");
-        [Test] public void DeserializeFieldFloatArray() => DeserializeField("FloatArray");
-        [Test] public void DeserializeFieldBoolArray() => DeserializeField("BoolArray");
-        [Test] public void DeserializeFieldStringArray() => DeserializeField("StringArray");
-        [Test] public void DeserializeFieldEnumArray() => DeserializeField("EnumArray");
-        [Test] public void DeserializeFieldColorArray() => DeserializeField("ColorArray");
-        [Test] public void DeserializeFieldPointArray() => DeserializeField("PointArray");
+        [Test] public void DeserializeFieldInt() => DeserializeField("Int", typeof(int));
+        [Test] public void DeserializeFieldFloat() => DeserializeField("Float", typeof(float));
+        [Test] public void DeserializeFieldBool() => DeserializeField("Bool", typeof(bool));
+        [Test] public void DeserializeFieldString() => DeserializeField("String", typeof(string));
+        [Test] public void DeserializeFieldEnum() => DeserializeField("Enum", typeof(string));
+        [Test] public void DeserializeFieldColor() => DeserializeField("Color", typeof(string));
+        [Test] public void DeserializeFieldPoint() => DeserializeField("Point", typeof(string));
+        [Test] public void DeserializeFieldIntArray() => DeserializeField("IntArray", typeof(int[]));
+        [Test] public void DeserializeFieldFloatArray() => DeserializeField("FloatArray", typeof(float[]));
+        [Test] public void DeserializeFieldBoolArray() => DeserializeField("BoolArray", typeof(bool[]));
+        [Test] public void DeserializeFieldStringArray() => DeserializeField("StringArray", typeof(string[]));
+        [Test] public void DeserializeFieldEnumArray() => DeserializeField("EnumArray", typeof(string[]));
+        [Test] public void DeserializeFieldColorArray() => DeserializeField("ColorArray", typeof(string[]));
+        [Test] public void DeserializeFieldPointArray() => DeserializeField("PointArray", typeof(string[]));
 
-        private void DeserializeField(string key)
+        private void DeserializeField(string key, Type type)
         {
             TextAsset fieldAsset = TestJsonLoader.LoadJson($"/LDtkMockField_{key}.json");
             
@@ -31,14 +32,16 @@ namespace Tests.Editor
             FieldInstance field = JsonConvert.DeserializeObject<FieldInstance>(fieldAsset.text);
             
             string identifier = field.Identifier;
-            string type = field.Type;
+            //string type = field.Type;
             object value = field.Value;
             
             Debug.Log($"identifier: {identifier}");
-            Debug.Log($"type: {type}");
+            //Debug.Log($"type: {type}");
             Debug.Log($"values: {value}");
 
-            Assert.False(value == null, "Field string array was null. Maybe this should not actually trigger failure.");
+            Assert.NotNull(value, "Field string array was null. Maybe this should not actually trigger failure.");
+            
+            Assert.IsAssignableFrom(type, value, "Object not assignable to type.");
         }
     }
 }
