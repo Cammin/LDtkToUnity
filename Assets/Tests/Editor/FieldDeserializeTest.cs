5a842b39f09359028478988d65148d6185bb6e6f
Cameron <cameo221@gmail.com>
2021-01-20 18:29:08 -0800

    applied some new change to the unit test for the new parsing

--- Assets/Tests/Editor/FieldDeserializeTest.cs
+++ Assets/Tests/Editor/FieldDeserializeTest.cs
@@ -13,25 +13,35 @@ namespace Tests.Editor
         [Test] public void DeserializeFieldFloat() => DeserializeField("Float", typeof(float));
         [Test] public void DeserializeFieldBool() => DeserializeField("Bool", typeof(bool));
         [Test] public void DeserializeFieldString() => DeserializeField("String", typeof(string));
-        [Test] public void DeserializeFieldEnum() => DeserializeField("Enum", typeof(string));
+        [Test] public void DeserializeFieldEnum() => DeserializeField("LocalEnum", typeof(string));
         [Test] public void DeserializeFieldColor() => DeserializeField("Color", typeof(string));
         [Test] public void DeserializeFieldPoint() => DeserializeField("Point", typeof(string));
-        [Test] public void DeserializeFieldIntArray() => DeserializeField("IntArray", typeof(int[]));
-        [Test] public void DeserializeFieldFloatArray() => DeserializeField("FloatArray", typeof(float[]));
-        [Test] public void DeserializeFieldBoolArray() => DeserializeField("BoolArray", typeof(bool[]));
-        [Test] public void DeserializeFieldStringArray() => DeserializeField("StringArray", typeof(string[]));
-        [Test] public void DeserializeFieldEnumArray() => DeserializeField("EnumArray", typeof(string[]));
-        [Test] public void DeserializeFieldColorArray() => DeserializeField("ColorArray", typeof(string[]));
-        [Test] public void DeserializeFieldPointArray() => DeserializeField("PointArray", typeof(string[]));
+        [Test] public void DeserializeFieldFilePath() => DeserializeField("FilePath", typeof(string));
+        
+        [Test] public void DeserializeFieldIntArray() => DeserializeField("Array<Int>", typeof(int[]));
+        [Test] public void DeserializeFieldFloatArray() => DeserializeField("Array<Float>", typeof(float[]));
+        [Test] public void DeserializeFieldBoolArray() => DeserializeField("Array<Bool>", typeof(bool[]));
+        [Test] public void DeserializeFieldStringArray() => DeserializeField("Array<String>", typeof(string[]));
+        [Test] public void DeserializeFieldEnumArray() => DeserializeField("Array<LocalEnum>", typeof(string[]));
+        [Test] public void DeserializeFieldColorArray() => DeserializeField("Array<Color>", typeof(string[]));
+        [Test] public void DeserializeFieldPointArray() => DeserializeField("Array<Point>", typeof(string[]));
+        [Test] public void DeserializeFieldFilePathArray() => DeserializeField("Array<FilePath>", typeof(string[]));
 
         private void DeserializeField(string key, Type type)
         {
-            TextAsset fieldAsset = TestJsonLoader.LoadJson($"/LDtkMockField_{key}.json");
+            TextAsset fieldAsset = TestJsonLoader.LoadJson($"/LDtkMockField_Project.json");
             
             //try deserializing field
-            FieldInstance field = JsonConvert.DeserializeObject<FieldInstance>(fieldAsset.text);
-            
-            string identifier = field.Identifier;
+            LdtkJson field = JsonConvert.DeserializeObject<LdtkJson>(fieldAsset.text);
+
+            FieldInstance[] fieldInstances = field.Levels[0].LayerInstances[0].EntityInstances[0].FieldInstances;
+
+            foreach (FieldInstance fieldInstance in fieldInstances)
+            {
+                Debug.Log(fieldInstance.Type);
+            }
+
+            /*string identifier = field.Identifier;
             //string type = field.Type;
             object value = field.Value;
             
@@ -41,7 +51,9 @@ namespace Tests.Editor
 
             Assert.NotNull(value, "Field string array was null. Maybe this should not actually trigger failure.");
             
-            Assert.IsAssignableFrom(type, value, "Object not assignable to type.");
+            Assert.IsAssignableFrom(type, value, "Object not assignable to type.");*/
         }
+        
+        
     }
 }
