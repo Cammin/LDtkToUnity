2fc1e8c35a67f8d711242a6136e03a1586c7546c
Cameron <cameo221@gmail.com>
2021-01-25 12:43:28 -0800

    expanded on the level display, optimized draw calls

--- Assets/LDtkUnity/Editor/CustomInspector/File/LDtkLevelFileEditor.cs
+++ Assets/LDtkUnity/Editor/CustomInspector/File/LDtkLevelFileEditor.cs
@@ -2,24 +2,60 @@
 using LDtkUnity.UnityAssets;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace LDtkUnity.Editor
 {
     [CustomEditor(typeof(LDtkLevelFile))]
     public class LDtkLevelFileEditor : LDtkJsonFileEditor<Level>
     {
+        private int? _layerCount = null;
+        private int? _intGridValueCount = null;
+        private int? _autoTileCount = null;
+        private int? _gridTileCount = null;
+        private int? _entityCount = null;
+        
         protected override void DrawInspectorGUI(Level level)
         {
             EditorGUILayout.TextField("Identifier", level.Identifier);
             
             LayerInstance[] layers = level.LayerInstances;
-            EditorGUILayout.LabelField($"{layers?.Length} Layers");
-
-            if (layers != null)
+            
+            if (layers == null)
             {
-                int tileCount = layers.Where(p => p.IsIntGridLayer).SelectMany(p => p.IntGrid).Count();
-                EditorGUILayout.LabelField($"{tileCount} Int Grid Values");
+                return;
+            }
+            
+            {
+                _layerCount ??= layers.Length;
+                string layerName = _layerCount == 1 ? "Layer" : "Layers";
+                EditorGUILayout.LabelField($"{_layerCount} {layerName}");
+            }
+
+            {
+                _intGridValueCount ??= layers.Where(p => p.IsIntGridLayer).SelectMany(p => p.IntGrid).Count();
+                string intGridValueName = _intGridValueCount == 1 ? "Int Grid Value" : "Int Grid Values";
+                EditorGUILayout.LabelField($"{_intGridValueCount} {intGridValueName}");
+            }
+
+            {
+                _autoTileCount ??= layers.Where(p => p.IsAutoTilesLayer).SelectMany(p => p.AutoLayerTiles)
+                    .Count();
+                string tileName = _autoTileCount == 1 ? "Auto Tile" : "Auto Tiles";
+                EditorGUILayout.LabelField($"{_autoTileCount} {tileName}");
+            }
+            
+            {
+                _gridTileCount = layers.Where(p => p.IsGridTilesLayer).SelectMany(p => p.GridTiles)
+                    .Count();
+                string tileName = _gridTileCount == 1 ? "Grid Tile" : "Grid Tiles";
+                EditorGUILayout.LabelField($"{_gridTileCount} {tileName}");
+            }
+                
+            {
+                _entityCount = layers.Where(p => p.IsEntityInstancesLayer).SelectMany(p => p.EntityInstances)
+                    .Count();
+                string entityName = _entityCount == 1 ? "Entity" : "Entities";
+                EditorGUILayout.LabelField($"{_entityCount} {entityName}");
             }
         }
     }
