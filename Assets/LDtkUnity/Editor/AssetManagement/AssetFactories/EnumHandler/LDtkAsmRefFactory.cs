56a4ef5f7057ed7d38326fce7d144e1993cf603c
Cameron <cameo221@gmail.com>
2021-03-05 12:49:59 -0800

    asm refs are correctly deleted if replaced by a new asmref, and deleted if none was specified

--- Assets/LDtkUnity/Editor/AssetManagement/AssetFactories/EnumHandler/LDtkAsmRefFactory.cs
+++ Assets/LDtkUnity/Editor/AssetManagement/AssetFactories/EnumHandler/LDtkAsmRefFactory.cs
@@ -12,23 +12,31 @@ namespace LDtkUnity.Editor
         
         public static void CreateAssemblyDefinitionReference(string folderPath, AssemblyDefinitionAsset asmDef)
         {
-            if (asmDef == null)
+
+            
+            string[] oldAsmDefRefGuids = AssetDatabase.FindAssets("t:AssemblyDefinitionReferenceAsset", new [] { folderPath });
+
+            //delete any amsrefs that are not supposed to be there
+            foreach (string oldGuid in oldAsmDefRefGuids)
             {
-                //don't make a file if none was inputted
-                return;
+                string pathToOldAsset = AssetDatabase.GUIDToAssetPath(oldGuid);
+                AssemblyDefinitionReferenceAsset asset = AssetDatabase.LoadAssetAtPath<AssemblyDefinitionReferenceAsset>(pathToOldAsset);
+                
+                //only delete if the asmdef was not assigned or is not the same name
+
+                if (asmDef != null && asset.name == asmDef.name)
+                {
+                    continue;
+                }
+                
+                AssetDatabase.DeleteAsset(pathToOldAsset);
             }
             
-            //todo find out a way to delete the old asm ref
-            //string[] pathToOldAsmDefRef = AssetDatabase.FindAssets(".asmref");
-            /Object[] assetsAtPath = AssetDatabase.LoadAllAssetsAtPath("Assets/");
-            foreach (Object o in assetsAtPath)
+            //don't make a file if none was inputted
+            if (asmDef == null)
             {
-                //if (o is AssemblyDefinitionReferenceAsset)
-                {
-                    Debug.Log(o.name);
-                }
+                return;
             }
-
 
             string asmDefPath = AssetDatabase.GetAssetPath(asmDef);
             GUID guid = AssetDatabase.GUIDFromAssetPath(asmDefPath);
@@ -42,6 +50,10 @@ namespace LDtkUnity.Editor
             {
                 streamWriter.Write(asmrefContents);
             }
+
+            //consider pinging? tried but decided against it
+            //AssemblyDefinitionReferenceAsset newAsset = AssetDatabase.LoadAssetAtPath<AssemblyDefinitionReferenceAsset>(asmRefPath);
+            //EditorGUIUtility.PingObject(newAsset);
         }
         
         
