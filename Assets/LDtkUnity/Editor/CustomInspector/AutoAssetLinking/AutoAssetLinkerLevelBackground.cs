84b95eaeb99ce1d93d203604994c25af11e4894c
Cameron <cameo221@gmail.com>
2021-03-21 17:32:26 -0700

    created the auto asset linker for level backgrounds in preperation for when that feature wil be implemented

---
+++ Assets/LDtkUnity/Editor/CustomInspector/AutoAssetLinking/AutoAssetLinkerLevelBackground.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace LDtkUnity.Editor
+{
+    public class AutoAssetLinkerLevelBackground : AutoAssetLinker<Level>
+    {
+        protected override GUIContent ButtonContent => new GUIContent()
+        {
+            text = "Auto-Assign Level Background",
+            tooltip = "Automatically assigns the references to the level's background texture by using the json's relative paths.",
+            //image = LDtkIconLoader.LoadLevelIcon()
+        };
+        protected override string GetRelPath(Level definition)
+        {
+            return definition.BgRelPath;
+        }
+    }
+}
