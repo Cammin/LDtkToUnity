fc0d2ee1b178eaa83b29bfcb2462fa11e37fa76d
Cameron <cameo221@gmail.com>
2021-03-29 14:53:43 -0700

    added/removed some scripts

---
+++ Assets/LDtkUnity/Editor/AssetManagement/AssetFactories/Tiles/LDtkTileCollectionFactoryParts.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace LDtkUnity.Editor
+{
+    public class LDtkTileCollectionFactoryParts
+    {
+        public readonly string Name;
+        public readonly Sprite Sprite;
+        public readonly Color Color;
+            
+        public LDtkTileCollectionFactoryParts(string name, Sprite sprite, Color color)
+        {
+            Name = name;
+            Sprite = sprite;
+            Color = color;
+        }
+    }
+}
---
+++ Assets/LDtkUnity/Runtime/Tools/LDtkKeyFormatUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace LDtkUnity
+{
+    public static class LDtkKeyFormatUtil
+    {
+        public static string IntGridValueFormat(LayerDefinition intGridLayerDef, IntGridValueDefinition def)
+        {
+            return $"{intGridLayerDef.Identifier}_{def.Value}";
+        }
+        
+        public static string TilesetKeyFormat(Texture2D tex, Vector2 srcRect)
+        {
+            return $"{tex.name}_x{srcRect.x}_y{srcRect.y}";
+        }
+    }
+}
