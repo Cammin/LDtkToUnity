28271e37124249d5f3ddce87d586a1caf788c884
Cameron <cameo221@gmail.com>
2021-03-28 23:15:17 -0700

    updated the Entities example scene with the level fields set up

---
+++ Assets/LDtkUnity/Samples~/Entities/Scripts/ExampleLevel.cs
@@ -0,0 +1,11 @@
+using LDtkUnity;
+using UnityEngine;
+
+namespace Samples.Entities
+{
+    public class ExampleLevel : MonoBehaviour
+    {
+        [LDtkField("tutorial")] public string _tutorial;
+        [LDtkField("textColor")] public Color _textColor;
+    }
+}
