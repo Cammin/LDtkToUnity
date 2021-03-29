42a995f425c8d5196896c81e59b3768399504b0f
Cameron <cameo221@gmail.com>
2021-03-29 15:20:25 -0700

    fixed ldtk drawer not drawing correctly for some types like multiline and point

--- Assets/LDtkUnity/Editor/LDtkFieldAttributeDrawer.cs
+++ Assets/LDtkUnity/Editor/LDtkFieldAttributeDrawer.cs
@@ -6,10 +6,16 @@ namespace LDtkUnity.Editor
     [CustomPropertyDrawer(typeof(LDtkFieldAttribute))]
     public class LDtkFieldAttributeDrawer : PropertyDrawer
     {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property);
+        }
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             label.tooltip = "This field is set by LDtk";
             label.image = LDtkIconLoader.LoadSimpleIcon();
+            
             EditorGUI.PropertyField(position, property, label);
         }
     }
