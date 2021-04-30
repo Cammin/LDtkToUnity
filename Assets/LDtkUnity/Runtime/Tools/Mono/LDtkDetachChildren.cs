4d821b6b3d5ec1816341a2a03906b8df97b8e838
Cameron <cameo221@gmail.com>
2021-04-30 00:30:03 -0700

    added icons to components that could use some

---
+++ Assets/LDtkUnity/Runtime/Tools/Mono/LDtkDetachChildren.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace LDtkUnity
+{
+    [AddComponentMenu(LDtkAddComponentMenu.ROOT + COMPONENT_NAME)]
+    [HelpURL(LDtkHelpURL.COMPONENT_DETACH_OBJECT)]
+    public class LDtkDetachChildren : MonoBehaviour
+    {
+        private const string COMPONENT_NAME = "Detach Children";
+
+        private void Awake()
+        {
+            transform.DetachChildren();
+            Destroy(this);
+        }
+    }
+}
