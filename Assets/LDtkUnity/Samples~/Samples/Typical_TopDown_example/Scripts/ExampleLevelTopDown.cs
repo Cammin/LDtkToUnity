131b2576a42890647a7a4aab49bc4c75a3affacc
Cameron <cameo221@gmail.com>
2021-05-15 23:31:34 -0700

    minor example fix

--- Assets/LDtkUnity/Samples~/Samples/Typical_TopDown_example/Scripts/ExampleLevelTopDown.cs
+++ Assets/LDtkUnity/Samples~/Samples/Typical_TopDown_example/Scripts/ExampleLevelTopDown.cs
@@ -5,7 +5,7 @@ namespace Samples.Samples.Typical_TopDown_example.Scripts
 {
     public class ExampleLevelTopDown : MonoBehaviour
     {
-        [LDtkField] public string enterMsg;
-        //[LDtkField] public  roomType;
+        [LDtkField] public string tutorial;
+        [LDtkField] public Color textColor;
     }
 }
