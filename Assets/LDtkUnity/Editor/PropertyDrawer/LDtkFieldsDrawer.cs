78eb29f26379466eaa03dc577642553ddb846705
Cameron <cameo221@gmail.com>
2021-05-16 20:17:21 -0700

    added a helpbox for the fields component

--- Assets/LDtkUnity/Editor/PropertyDrawer/LDtkFieldsDrawer.cs
+++ Assets/LDtkUnity/Editor/PropertyDrawer/LDtkFieldsDrawer.cs
@@ -6,10 +6,16 @@ namespace LDtkUnity.Editor
     [CustomEditor(typeof(LDtkFields))]
     public class LDtkFieldsDrawer : UnityEditor.Editor
     {
+        private static readonly GUIContent HelpBox = new GUIContent()
+        {
+            text = "Access the fields by gaining reference to this component with GetComponent<LDtkFields>();",
+        };
+
         public override void OnInspectorGUI()
         {
+            EditorGUILayout.HelpBox(HelpBox, true);
+            
             SerializedProperty fieldsProp = serializedObject.FindProperty(LDtkFields.PROP_FIELDS);
-
             EditorGUILayout.PropertyField(fieldsProp);
         }
     }
