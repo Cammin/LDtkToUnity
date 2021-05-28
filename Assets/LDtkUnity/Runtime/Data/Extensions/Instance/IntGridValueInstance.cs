db6d50194aa3d171de083c1467647870a09f023c
Cameron <cameo221@gmail.com>
2021-05-27 20:25:07 -0700

    excluding a few more data classes with no extended functionality

---
+++ Assets/LDtkUnity/Runtime/Data/Extensions/Instance/IntGridValueInstance.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.Internal;
+
+namespace LDtkUnity
+{
+    [ExcludeFromDocs]//keep like this until we add custom functionality
+    public partial class IntGridValueInstance
+    {
+        
+    }
+}
