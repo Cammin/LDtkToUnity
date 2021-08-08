2369ee44fbcc01e66b318705623f8ff3ed1e03f0
Cammin <cameo221@gmail.com>
2021-08-08 16:41:42 -0700

    changed an error and warning to be more helpful

--- Assets/LDtkUnity/Editor/CustomEditor/Importer/LDtkProjectImporterEditor.cs
+++ Assets/LDtkUnity/Editor/CustomEditor/Importer/LDtkProjectImporterEditor.cs
@@ -119,7 +119,7 @@ namespace LDtkUnity.Editor
             Object jsonAsset = jsonProp.objectReferenceValue;
             if (jsonAsset == null)
             {
-                Debug.LogError("LDtk: Json asset is null, it's never expected to happen. Importer error?");
+                Debug.LogError("LDtk: Json asset is null, resulting in the importer inspector not drawing. This is not expected to happen. Check if there are any import errors and report to the developer so that it's fixed.");
                 return false;
             }
             
