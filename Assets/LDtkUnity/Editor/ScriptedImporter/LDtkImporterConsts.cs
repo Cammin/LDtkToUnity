d0d2416d88d2aff787be5da5225512507c5f7c87
Cammin <cameo221@gmail.com>
2021-10-25 19:52:37 -0700

    potentially fixed the prefab bug when reimporting an ldtk file

--- Assets/LDtkUnity/Editor/ScriptedImporter/LDtkImporterConsts.cs
+++ Assets/LDtkUnity/Editor/ScriptedImporter/LDtkImporterConsts.cs
@@ -9,8 +9,9 @@
         public const string LEVEL_EXT = "ldtkl";
 
         //choosing a high import order so that any prefab changes respond correctly for the import process, whether prefabs are modified internally in unity or externally (eg. source control)
+        //import order is not documented much, but prefab order is 500/501 https://forum.unity.com/threads/understanding-import-order-of-native-unity-asset-types.1187845/
         public const int LEVEL_ORDER = 10;
-        public const int PROJECT_ORDER = 11;
+        public const int PROJECT_ORDER = 502;
         
         public const int DEFAULT_PPU = 16;
     }
