2fa3dd76220ec34861aba0d64929407c5a34899c
Cameron Krebbers <cameo221@gmail.com>
2021-12-27 16:45:54 -0800

    package updates, added test for importing a level, testing using different unity version to test build/tests

--- Assets/Tests/Editor/Tests/TestImport.cs
+++ Assets/Tests/Editor/Tests/TestImport.cs
@@ -11,5 +11,12 @@ namespace Tests.Editor
             string path = "Assets/Samples/Samples/Test_file_for_API_showing_all_features.ldtk";
             AssetDatabase.ImportAsset(path);
         }
+        
+        [Test]
+        public static void ImportLevel()
+        {
+            string path = "Assets/Samples/Samples/Test_file_for_API_showing_all_features/0000-Level_0.ldtkl";
+            AssetDatabase.ImportAsset(path);
+        }
     }
 }
