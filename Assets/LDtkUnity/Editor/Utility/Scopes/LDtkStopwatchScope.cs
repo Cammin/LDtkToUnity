673daf64288415cf7318f5109573e73e30b1c257
Cammin <cameo221@gmail.com>
2022-01-09 14:43:11 -0800

    added a stopwatchScript

---
+++ Assets/LDtkUnity/Editor/Utility/Scopes/LDtkStopwatchScope.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+namespace LDtkUnity.Editor
+{
+    public class LDtkStopwatchScope : IDisposable
+    {
+        private readonly Stopwatch _stopwatch;
+        private string _name;
+
+        public LDtkStopwatchScope(string name)
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+            Debug.Log($"{_name}:{_stopwatch.ElapsedMilliseconds}ms");
+        }
+    }
+}
