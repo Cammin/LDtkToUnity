7f8c009c217f50d5115e282dfc3c9001f30429c1
Cammin <cameo221@gmail.com>
2022-01-30 17:16:33 -0800

    potentially fixed any spite atlas related crashes

--- Assets/LDtkUnity/Editor/ScriptedImporter/LDtkProjectImporterAtlasPacker.cs
+++ Assets/LDtkUnity/Editor/ScriptedImporter/LDtkProjectImporterAtlasPacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.U2D;
 using UnityEngine;
@@ -9,57 +10,55 @@ namespace LDtkUnity.Editor
     internal class LDtkProjectImporterAtlasPacker
     {
         private readonly SpriteAtlas _atlas;
-        private readonly string _assetPath;
+        private readonly Sprite[] _subAssets;
+        
+        private static readonly List<SpriteAtlas> Atlases = new List<SpriteAtlas>();
+        private static bool _hasPacked;
 
-        public LDtkProjectImporterAtlasPacker(SpriteAtlas atlas, string assetPath)
+        public LDtkProjectImporterAtlasPacker(SpriteAtlas atlas, Sprite[] spriteAssets)
         {
             _atlas = atlas;
-            _assetPath = assetPath;
+            _subAssets = spriteAssets;
+        }
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void ResetStuff()
+        {
+            _hasPacked = false;
         }
 
         public void TryPack()
         {
             Object[] prevPackables = _atlas.GetPackables();
-            
-            //there is a unity issue where we try to pack in 2019.3, but a lost reference to a sprite trying to get packed, results in an editor crash. so make sure there is nothing null in the previous packables
-#if !UNITY_2019_4_OR_NEWER
-            if (!prevPackables.IsNullOrEmpty() && prevPackables.Any(p => p == null))
-            {
-                Debug.LogWarning($"LDtk: Did not pack sprite atlas \"{_atlas.name}\"; A Unity 2019.3 bug could have crashed the editor when packing the atlas. Try emptying the atlas of any references and reimport the LDtk project.", _atlas);
-                return;
-            }
-#endif
-
-            //if only 2019.4, there's an issue when reimporting all.
-#if UNITY_2019_4_OR_NEWER && !UNITY_2020_1_OR_NEWER
-            Debug.LogWarning($"LDtk: Did not pack sprite atlas \"{_atlas.name}\"; A Unity 2019.4 bug could have crashed/locked the editor when packing the atlas. You will need to manually pack the atlas.", _atlas);
-#else
             Pack(prevPackables);
-#endif
+            //there is a unity issue where we try to pack in 2019.3, but a lost reference to a sprite trying to get packed, results in an editor crash. so make sure there is nothing null in the previous packables
         }
 
+        
+        //dirty zombie code is for potential optimizing later
         private void Pack(Object[] prevPackables)
         {
-            Object[] subAssets = AssetDatabase.LoadAllAssetRepresentationsAtPath(_assetPath);
+            Object[] newSprites = _subAssets.Cast<Object>().ToArray();
+            
+            //Object[] subAssets = AssetDatabase.LoadAllAssetRepresentationsAtPath(_assetPath);
 
             //load artifacts. the local reference is lost after the delay call
-            LDtkArtifactAssets artifacts = (LDtkArtifactAssets)subAssets.FirstOrDefault(t => t is LDtkArtifactAssets);
-            if (artifacts == null)
-            {
-                Debug.LogError("LDtk: Import issue, was not able to load the artifact asset. Not packing to atlas");
-                return;
-            }
+            //LDtkArtifactAssets artifacts = (LDtkArtifactAssets)_subAssets.FirstOrDefault(t => t is LDtkArtifactAssets);
+            //if (artifacts == null)
+            //{
+                //Debug.LogError("LDtk: Import issue, was not able to load the artifact asset. Not packing to atlas");
+                //return;
+            //}
 
             //don't pack backgrounds
-            Object[] newSprites = subAssets.Distinct()
-                .Where(p => p != null && p is Sprite sprite && !artifacts.ContainsBackground(sprite))
-                .OrderBy(p => p.name).ToArray();
+                //.Where(p => p != null && p is Sprite sprite && !artifacts.ContainsBackground(sprite))
+                //.OrderBy(p => p.name).ToArray();
 
             //compare with existing sprites to make sure if it's even necessary to pack. also may help keep git cleaner
-            Object[] prevSprites = prevPackables.Where(p => p != null && p is Sprite).ToArray();
+            //Object[] prevSprites = prevPackables.Where(p => p != null && p is Sprite).ToArray();
 
             //only remove and re-add to the sprite atlas if any sprite assets are different from the last one. regardless, pack in case the texture was modified
-            if (!AreEqualSpriteArrays(prevSprites, newSprites))
+            //if (!AreEqualSpriteArrays(prevSprites, newSprites))
             {
                 //remove existing
                 _atlas.Remove(prevPackables);
@@ -71,7 +70,26 @@ namespace LDtkUnity.Editor
             //todo check if the texture was changed, or if there was a reimport as a result of a texture, in order to detect if we should spend the time to pack textures if it's really necessary.
 
             //automatically pack it
-            SpriteAtlasUtility.PackAtlases(new[] { _atlas }, EditorUserBuildSettings.activeBuildTarget);
+            DoPackAction();
+        }
+
+        private void DoPackAction()
+        {
+            Atlases.Add(_atlas);
+
+            EditorApplication.delayCall += () =>
+            {
+                if (_hasPacked)
+                {
+                    return;
+                }
+
+                _hasPacked = true;
+
+                SpriteAtlasUtility.PackAtlases(Atlases.ToArray(), EditorUserBuildSettings.activeBuildTarget);
+
+                EditorApplication.delayCall += () => { _hasPacked = false; };
+            };
         }
 
         private static bool AreEqualSpriteArrays(Object[] a1, Object[] a2)
