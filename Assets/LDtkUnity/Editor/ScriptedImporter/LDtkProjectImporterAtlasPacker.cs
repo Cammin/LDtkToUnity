2b9a67ef84e4a142cb732a84215102f19cb5adbc
Cammin <cameo221@gmail.com>
2022-02-06 01:24:20 -0800

    tried playing with better saving for sprite atlases via OnWillSaveAssets

--- Assets/LDtkUnity/Editor/ScriptedImporter/LDtkProjectImporterAtlasPacker.cs
+++ Assets/LDtkUnity/Editor/ScriptedImporter/LDtkProjectImporterAtlasPacker.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.U2D;
+using Object = UnityEngine.Object;
 
 namespace LDtkUnity.Editor
 {
@@ -29,15 +31,14 @@ namespace LDtkUnity.Editor
 
         public void TryPack()
         {
-            Object[] prevPackables = _atlas.GetPackables();
-            Pack(prevPackables);
+            Pack();
             //there is a unity issue where we try to pack in 2019.3, but a lost reference to a sprite trying to get packed, results in an editor crash. so make sure there is nothing null in the previous packables
         }
-
         
         //dirty zombie code is for potential optimizing later
-        private void Pack(Object[] prevPackables)
+        private void Pack()
         {
+            Object[] prevPackables = _atlas.GetPackables();
             Object[] newSprites = _subAssets.Cast<Object>().ToArray();
             
             //Object[] subAssets = AssetDatabase.LoadAllAssetRepresentationsAtPath(_assetPath);
@@ -65,6 +66,7 @@ namespace LDtkUnity.Editor
 
                 //add sorted sprites
                 _atlas.Add(newSprites);
+                Debug.Log($"atlas add {string.Join(", ", newSprites.Cast<Object>())}, 7");
             }
 
             //todo check if the texture was changed, or if there was a reimport as a result of a texture, in order to detect if we should spend the time to pack textures if it's really necessary.
@@ -72,24 +74,72 @@ namespace LDtkUnity.Editor
             //automatically pack it
             DoPackAction();
         }
-
+        
         private void DoPackAction()
         {
             Atlases.Add(_atlas);
+            EditorApplication.delayCall += PackAction;
+        }
 
-            EditorApplication.delayCall += () =>
+        private static void PackAction()
+        {
+            if (_hasPacked)
             {
-                if (_hasPacked)
+                return;
+            }
+            _hasPacked = true;
+
+            SpriteAtlasUtility.PackAtlases(Atlases.ToArray(), EditorUserBuildSettings.activeBuildTarget);
+            
+            foreach (SpriteAtlas atlas in Atlases)
+            {
+                EditorUtility.SetDirty(atlas);
+            }
+
+            EditorApplication.delayCall += ResetAndSave;
+        }
+
+        private static void ResetAndSave()
+        {
+            try
+            {
+                SaveAtlases();
+            }
+            finally
+            {
+                _hasPacked = false;
+                Atlases.Clear();
+            }
+        }
+
+        private static void SaveAtlases()
+        {
+            foreach (SpriteAtlas atlas in Atlases)
+            {
+
+#if UNITY_2020_3_OR_NEWER
+                Debug.Log($"Saving atlas: \"{atlas.name}\"");
+                AssetDatabase.SaveAssetIfDirty(atlas);
+#else
+                AssetDatabase.SaveAssets();
+#endif
+            }
+        }
+
+        private class AtlasSaveChecker : UnityEditor.AssetModificationProcessor
+        {
+            private static string[] OnWillSaveAssets(string[] paths)
+            {
+                if (!_hasPacked || Atlases.IsNullOrEmpty())
                 {
-                    return;
+                    return paths;
                 }
 
-                _hasPacked = true;
+                string[] atlasesToSave = Atlases.Select(AssetDatabase.GetAssetPath).ToArray();
+                Debug.Log($"Saving atlases:\"{string.Join("\",\n\"", atlasesToSave)}\"");
+                return atlasesToSave;
 
-                SpriteAtlasUtility.PackAtlases(Atlases.ToArray(), EditorUserBuildSettings.activeBuildTarget);
-
-                EditorApplication.delayCall += () => { _hasPacked = false; };
-            };
+            }
         }
 
         private static bool AreEqualSpriteArrays(Object[] a1, Object[] a2)
