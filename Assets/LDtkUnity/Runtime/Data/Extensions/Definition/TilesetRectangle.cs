885ece3d632056f9933e8b54e451e9de6a534e7f
Cammin <cameo221@gmail.com>
2022-02-24 00:30:11 -0800

    schema: the actual TilesetRectangle is now included

--- Assets/LDtkUnity/Runtime/Data/Extensions/Definition/TilesetRectangle.cs
+++ Assets/LDtkUnity/Runtime/Data/Extensions/Definition/TilesetRectangle.cs
@@ -21,41 +21,4 @@ namespace LDtkUnity
             return JsonConvert.DeserializeObject<TilesetRectangle>(json, Converter.Settings);
         }
     }
-    
-    //the schema export. It wasn't generated automatically.
-    /// <summary>
-    /// This object represents a custom sub rectangle in a Tileset image.
-    /// </summary>
-    public partial class TilesetRectangle
-    {
-        /// <value>
-        /// Height in pixels
-        /// </value>
-        [JsonProperty("h")]
-        public long H { get; set; }
-        
-        /// <value>
-        /// UID of the tileset
-        /// </value>
-        [JsonProperty("tilesetUid")]
-        public long TilesetUid { get; set; }
-        
-        /// <value>
-        /// Width in pixels
-        /// </value>
-        [JsonProperty("w")]
-        public long W { get; set; }
-
-        /// <value>
-        /// X pixels coordinate of the top-left corner in the Tileset image
-        /// </value>
-        [JsonProperty("x")]
-        public long X { get; set; }
-
-        /// <value>
-        /// Y pixels coordinate of the top-left corner in the Tileset image
-        /// </value>
-        [JsonProperty("y")]
-        public long Y { get; set; }
-    }
 }
