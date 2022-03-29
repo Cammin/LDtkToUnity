35e00d20f8333e93a4aaa08aa0e1dd7695c353b8
Cammin <cameo221@gmail.com>
2022-03-28 19:53:00 -0700

    added extended nine slice functionality

--- Assets/LDtkUnity/Runtime/Data/Extensions/Definition/EntityDefinition.cs
+++ Assets/LDtkUnity/Runtime/Data/Extensions/Definition/EntityDefinition.cs
@@ -28,5 +28,12 @@ namespace LDtkUnity
         /// Pivot coords (from 0 to 1 for both axes)
         /// </value>
         [JsonIgnore] public Vector2 UnityPivot => new Vector2((float)PivotX, (float)PivotY);
+        
+        /// <value>
+        /// An array of 4 dimensions for the up/right/down/left borders (in this order) when using
+        /// 9-slice mode for `tileRenderMode`.<br/>  If the tileRenderMode is not NineSlice, then
+        /// this array is empty.<br/>  See: https://en.wikipedia.org/wiki/9-slice_scaling
+        /// </value>
+        [JsonIgnore] public Rect UnityNineSliceBorders => NineSliceBorders.IsNullOrEmpty() ? Rect.zero : NineSliceBorders.ToRect();
     }
 }
