12317ab5334bb3c2eb2fe0d5cfb422444ea108a6
Cammin <cameo221@gmail.com>
2022-04-17 22:01:53 -0700

    some reconfiguration to prevent errors in the project itself

--- Assets/LDtkUnity/Editor/Builders/LDtkProjectImporterFactory.cs
+++ Assets/LDtkUnity/Editor/Builders/LDtkProjectImporterFactory.cs
@@ -13,12 +13,7 @@ namespace LDtkUnity.Editor
 
         public void Import(LdtkJson json)
         {
-            //set the data class's levels correctly, regardless if they are external levels or not
-            RestructureJson(json);
-            
-            LDtkProjectBuilder builder = new LDtkProjectBuilder(_importer, json);
-            builder.BuildProject();
-            GameObject projectGameObject = builder.RootObject;
+            GameObject projectGameObject = GetProjectGameObject(json);
 
             if (projectGameObject == null)
             {
@@ -29,7 +24,24 @@ namespace LDtkUnity.Editor
             _importer.ImportContext.AddObjectToAsset("rootGameObject", projectGameObject, LDtkIconUtility.LoadProjectFileIcon());
             _importer.ImportContext.SetMainObject(projectGameObject);
         }
-        
+
+        private GameObject GetProjectGameObject(LdtkJson json)
+        {
+            if (json.ExternalLevels)
+            {
+                //don't build it if it's an external levels project, and if so, then we don't have to 
+                return new GameObject(_importer.AssetName);
+            }
+            
+            //set the data class's levels correctly, regardless if they are external levels or not
+            RestructureJson(json);
+
+            LDtkProjectBuilder builder = new LDtkProjectBuilder(_importer, json);
+            builder.BuildProject();
+            GameObject projectGameObject = builder.RootObject;
+            return projectGameObject;
+        }
+
         private void RestructureJson(LdtkJson project)
         {
             //we wanna modify the json and serialize it back to that it's usable later with it's completeness regardless of external levels
