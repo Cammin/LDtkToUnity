ffd2ac3935b4a332d0b00e26dae84adc4ca36cd9
Cammin <cameo221@gmail.com>
2022-07-24 00:15:13 -0700

    fixed text for test api sample

--- Assets/LDtkUnity/Samples~/Samples/Test_file_for_API_showing_all_features/ExampleLabels.cs
+++ Assets/LDtkUnity/Samples~/Samples/Test_file_for_API_showing_all_features/ExampleLabels.cs
@@ -6,17 +6,36 @@ namespace Samples.Test_file_for_API_showing_all_features
     public class ExampleLabels : MonoBehaviour, ILDtkImportedFields
     {
         [SerializeField] private TextMesh _textMesh = null;
+        [SerializeField] private SpriteRenderer _textBg = null;
         
         public void OnLDtkImportFields(LDtkFields fields)
         {
-            if (_textMesh == null)
+            TextMesh(fields);
+            Background(fields);
+        }
+
+        private void Background(LDtkFields fields)
+        {
+            if (_textBg == null)
             {
-                Debug.LogWarning($"LDtk Sample: An entity's referenced component was null. This can happen when importing the examples for the first time. Try reimporting again to fix the samples.\n{name}", gameObject);
+                Debug.LogWarning($"LDtk Sample: An entity's referenced component was null. This can happen when importing the examples for the first time. Try reimporting again to fix the samples.\n{name}",
+                    gameObject);
                 return;
             }
             
+            _textBg.color = fields.GetColor("color");
+        }
+
+        private void TextMesh(LDtkFields fields)
+        {
+            if (_textMesh == null)
+            {
+                Debug.LogWarning($"LDtk Sample: An entity's referenced component was null. This can happen when importing the examples for the first time. Try reimporting again to fix the samples.\n{name}",
+                    gameObject);
+                return;
+            }
+
             _textMesh.text = fields.GetMultiline("text");
-            _textMesh.color = fields.GetColor("color");
         }
     }
 }
