dace7dd7ebff3606fde91bde90843ab4d4628dcd
Cameron <cameo221@gmail.com>
2022-10-30 22:13:11 -0700

    minor test edit

--- Assets/Tests/EditMode/Tests/TestVersioning.cs
+++ Assets/Tests/EditMode/Tests/TestVersioning.cs
@@ -27,10 +27,7 @@ namespace LDtkUnity.Tests
         public void TryOldVersions(string ver)
         {
             LogAssert.Expect(LogType.Error, new Regex($"<color={LDtkDebug.GetStringColor()}>LDtk:</color> The version of the project*"));
-            
-            LogAssert.Expect(LogType.Error, "There is already a [ITestService.Test Service 1] registered!");
-            LogAssert.Expect(LogType.Error, new Regex("There is already a \\[ITestService.Test Service 1] registered!"));
-            
+
             LDtkProjectImporter.CheckOutdatedJsonVersion(ver, "test");
         }
         [Test, TestCaseSource(nameof(_versions))]
