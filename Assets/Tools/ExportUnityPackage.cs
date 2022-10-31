a4b06579258efe559681d5a86c2b68ac01c25d85
Cameron <cameo221@gmail.com>
2022-10-31 00:15:06 -0700

    experiment with export unity package. best to export this for later versions when system text json is in so we don't run into dependency issues for newtonsoft when installing the package

---
+++ Assets/Tools/ExportUnityPackage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace LDtkUnity.Editor
+{
+    internal static class ExportUnityPackage
+    {
+        [Serializable]
+        public class PackageJson
+        {
+            public string name;
+            public string version;
+        }
+        
+        [MenuItem("LDtkUnity/Export .unitypackage")]
+        private static void ExportPackage()
+        {
+            string text = File.ReadAllText("Assets/LDtkUnity/package.json");
+            
+            text = text.Replace("\n", string.Empty); 
+            text = text.Replace("\r", string.Empty); 
+            text = text.Replace("\t", string.Empty);
+
+            PackageJson info = JsonUtility.FromJson<PackageJson>(text);
+            
+            LDtkPathUtility.TryCreateDirectory("ExportUnityPackage");
+            AssetDatabase.ExportPackage("Assets/LDtkUnity", $"ExportUnityPackage/{info.name}-{info.version}.unitypackage", ExportPackageOptions.Recurse);
+        }
+    }
+}
