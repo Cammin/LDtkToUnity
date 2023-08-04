026913d0e5c9f3a88d7a9e13da250075680c4c25
Cameron <cameo221@gmail.com>
2023-08-03 23:06:04 -0700

    added test for custom backup paths

---
+++ Assets/Tests/Misc/OtherTechTests/TestAllFields/TestAllFieldsAssetReferences.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Samples.TestAllFields
+{
+    public class TestAllFieldsAssetReferences : MonoBehaviour
+    {
+        public List<Sprite> References;
+    }
+}
