1c5416e771bc79baf5f7479b7851be86399937e9
Cameron <cameo221@gmail.com>
2023-08-20 23:33:19 -0700

    made a linux version of making a symbolic link

---
+++ Assets/CreateSymbolicLink_Linux.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+root=$(dirname "$0")
+startPath="${root}/Samples"
+destPath="${root}/LDtkUnity/Samples~"
+
+echo "$root"
+echo StartPath is: "$startPath"
+echo DestPath is: "$destPath"
+
+ln -s "$destPath" "$startPath"
+
+read -p "Press any key to continue "
