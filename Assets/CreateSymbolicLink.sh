00f8e7250edfcc160f6b91a671e966594ec82489
Cameron <cameo221@gmail.com>
2023-08-21 02:32:49 -0700

    list everything please

--- Assets/CreateSymbolicLink.sh
+++ Assets/CreateSymbolicLink.sh
@@ -2,10 +2,19 @@ root=$(dirname "$0")
 startPath="${root}/Samples"
 destPath="${root}/LDtkUnity/Samples~"
 
+
+
 echo "$root"
 echo StartPath is: "$startPath"
 echo DestPath is: "$destPath"
 
 ln -s "$destPath" "$startPath"
+
+
+echo "Listing everything!"
+echo "----------"
+ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
+
+
 
 #read -p "Press any key to continue "
