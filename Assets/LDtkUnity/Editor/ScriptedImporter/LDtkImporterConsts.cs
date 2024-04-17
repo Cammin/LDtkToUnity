8c591595fb5c0f14b7cd956e366ef9aecca20ce7
Cameron <cameo221@gmail.com>
2024-04-17 03:41:45 -0700

    updated the export app to get all tiles properly now

--- Assets/LDtkUnity/Editor/ScriptedImporter/LDtkImporterConsts.cs
+++ Assets/LDtkUnity/Editor/ScriptedImporter/LDtkImporterConsts.cs
@@ -6,7 +6,7 @@
         public const int LEVEL_VERSION = 13;
         public const int TILESET_VERSION = 4;
         public const string MINIMUM_JSON_VERSION = "1.5.0";
-        public const string EXPORT_APP_VERSION_REQUIRED = "1.5.2.0";
+        public const string EXPORT_APP_VERSION_REQUIRED = "1.5.3.0";
 
         public const string PROJECT_EXT = "ldtk";
         public const string LEVEL_EXT = "ldtkl";
