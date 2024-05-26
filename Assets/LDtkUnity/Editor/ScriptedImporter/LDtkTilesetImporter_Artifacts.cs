98a585cf69bcea4b70be7d96cb94a36ada64ea30
Cameron <cameo221@gmail.com>
2024-05-26 04:56:30 -0700

    fix: Compile error on versions 2020.1 and below

--- Assets/LDtkUnity/Editor/ScriptedImporter/LDtkTilesetImporter_Artifacts.cs
+++ Assets/LDtkUnity/Editor/ScriptedImporter/LDtkTilesetImporter_Artifacts.cs
@@ -6,6 +6,12 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+#if UNITY_2020_2_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
+using UnityEditor.Experimental.AssetImporters;
+#endif
+
 namespace LDtkUnity.Editor
 {
     /// <summary>
