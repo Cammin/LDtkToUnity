47dddfbf2dc2c926af69bd2f15aee5d924166be1
Cameron <cameo221@gmail.com>
2024-05-26 05:00:21 -0700

    fix: Compile error on versions 2020.1 and below (again)

--- Assets/LDtkUnity/Editor/ScriptedImporter/LDtkTilesetImporter_TextureGeneration.cs
+++ Assets/LDtkUnity/Editor/ScriptedImporter/LDtkTilesetImporter_TextureGeneration.cs
@@ -3,8 +3,13 @@ using System.Linq;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEditor;
-using UnityEditor.AssetImporters;
 using UnityEngine;
+
+#if UNITY_2020_2_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
+using UnityEditor.Experimental.AssetImporters;
+#endif
 
 namespace LDtkUnity.Editor
 {
