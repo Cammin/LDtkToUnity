73040e579a1ac4c18d12229d2e8b83515052618d
Cameron <cameo221@gmail.com>
2025-04-16 10:50:16 -0700

    feat: Added new type checks for FieldDefinition among other small json data changes

--- Assets/LDtkUnity/Runtime/Data/Extensions/Definition/FieldDefinition.cs
+++ Assets/LDtkUnity/Runtime/Data/Extensions/Definition/FieldDefinition.cs
@@ -12,5 +12,62 @@ namespace LDtkUnity
         [IgnoreDataMember] public TilesetDefinition Tileset => TilesetUid == null ? null : LDtkUidBank.GetUidData<TilesetDefinition>(TilesetUid.Value);
         
         [IgnoreDataMember] public Color UnityEditorDisplayColor => EditorDisplayColor.ToColor();
+        
+        /// <value>
+        /// Returns if this field (or array element) type is an Int. 
+        /// </value>
+        [IgnoreDataMember] public bool IsInt => Type == "Int" || Type == "Array<Int>";
+        
+        /// <value>
+        /// Returns if this field (or array element) type is a Float. 
+        /// </value>
+        [IgnoreDataMember] public bool IsFloat => Type == "Float" || Type == "Array<Float>";
+        
+        /// <value>
+        /// Returns if this field (or array element) type is a Bool. 
+        /// </value>
+        [IgnoreDataMember] public bool IsBool => Type == "Bool" || Type == "Array<Bool>";
+        
+        /// <value>
+        /// Returns if this field (or array element) type is a String. 
+        /// </value>
+        [IgnoreDataMember] public bool IsString => Type == "String" || Type == "Array<String>";
+
+        /// <value>
+        /// Returns if this field (or array element) type is MultiLines. 
+        /// </value>
+        [IgnoreDataMember] public bool IsMultilines => Type == "Multilines" || Type == "Array<Multilines>";
+        
+        /// <value>
+        /// Returns if this field (or array element) type is a FilePath. 
+        /// </value>
+        [IgnoreDataMember] public bool IsFilePath => Type == "FilePath" || Type == "Array<FilePath>";
+        
+        /// <value>
+        /// Returns if this field (or array element) type is a Color. 
+        /// </value>
+        [IgnoreDataMember] public bool IsColor => Type == "Color" || Type == "Array<Color>";
+
+        /// <value>
+        /// Returns if this field (or array element) type is an Enum. 
+        /// </value>
+        [IgnoreDataMember]
+        public bool IsEnum => Type.Contains("LocalEnum.") || Type.Contains("ExternEnum.");
+
+        /// <value>
+        /// Returns if this field (or array element) type is a Tile. 
+        /// </value>
+        [IgnoreDataMember] public bool IsTile => Type == "Tile" || Type == "Array<Tile>";    
+        
+        /// <value>
+        /// Returns if this field (or array element) type is a EntityRef. 
+        /// </value>
+        [IgnoreDataMember] public bool IsEntityRef => Type == "EntityRef" || Type == "Array<EntityRef>";    
+        
+        /// <value>
+        /// Returns if this field (or array element) type is a Point. 
+        /// </value>
+        [IgnoreDataMember] public bool IsPoint => Type == "Point" || Type == "Array<Point>";
+
     }
 }
