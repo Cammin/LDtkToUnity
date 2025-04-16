7786197c2367a94f18ef95d366c0c9c2098490f6
Cameron <cameo221@gmail.com>
2025-04-16 13:17:18 -0700

    chore: Importer version to ensure up to date

--- Assets/LDtkUnity/Editor/ScriptedImporter/LDtkImporterConsts.cs
+++ Assets/LDtkUnity/Editor/ScriptedImporter/LDtkImporterConsts.cs
@@ -2,8 +2,8 @@
 {
     internal static class LDtkImporterConsts
     {
-        public const int PROJECT_VERSION = 32;
-        public const int LEVEL_VERSION = 16;
+        public const int PROJECT_VERSION = 33;
+        public const int LEVEL_VERSION = 17;
         public const int TILESET_VERSION = 8;
         public const int CONFIG_VERSION = 0;
         
